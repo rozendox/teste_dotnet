1ebce34a45faa3c0902a00246d9b7fabd4844101
Gabriel-Rozendo <gabrielsg236@gmail.com>
2026-05-26 15:21:28 -0300

    feat: implementacao dos dtos de request e reponse

--- DTOs/CriarOrcamentoRequest.cs
+++ DTOs/CriarOrcamentoRequest.cs
@@ -5,3 +5,18 @@ namespace teste_dotnet.DTOs;
 /// <summary>
 /// DTO utilizado para receber os dados de criação de orçamento.
 /// </summary>
+
+
+public class CriarOrcamentoRequest
+{
+    public int ClienteId { get; set; }
+    public int VeiculoId { get; set; }
+    public List<OrcamentoItemRequest> Itens { get; set; } = new();
+}
+
+public class OrcamentoItemRequest
+{
+    public string Descricao { get; set; }
+    public int Quantidade { get; set; }
+    public decimal ValorUnitario { get; set; }
+}
